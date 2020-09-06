a70900d38f7a73ba9afa01c6e948fbb932d8935d
github username <github email address>
2020-09-06 10:41:36 -0700

    create post function

--- index.js
+++ index.js
@@ -36,10 +36,34 @@ function getGroceries() {
 // function grabs all the values for the inputs on the form
 function createFormHandler(e) {
     e.preventDefault()
-    const marketId = parseInt(document.querySelector("#markets").value)
-    const groceryItemInput = document.querySelector("#input-grocery-item").value
-    const qtyInput = document.querySelector("#input-qty").value
-    const notesInput = document.querySelector("#input-notes").value
+    const marketId = parseInt(document.querySelector('#markets').value)
+    const groceryItemInput = document.querySelector('#input-grocery-item').value
+    const qtyInput = document.querySelector('#input-qty').value
+    const notesInput = document.querySelector('#input-notes').value
+    postFetch(marketId, groceryItemInput, qtyInput, notesInput)
 }
 
+function postFetch(market_id, groceryItem, qty, notes) {
 
+    const bodyData = {market_id, groceryItem, qty, notes}
+    
+    fetch(baseUrl, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(grocery => { 
+        const groceryData = grocery.data.attributes
+        const groceryMarkUp = `
+        <div data-id=${grocery.id}>
+            <h3>${groceryData.market.name}</h3>
+            <p>${grocery.groceryItem}</p>
+            <p>${grocery.qty}</p>
+            <p>${grocery.notes}</p>
+            <button data-id=${groceryData.id}>Edit</button>
+        </div>
+        `;
+        document.querySelector('#grocery-container').innerHTML +=groceryMarkUp
+    })
+}
