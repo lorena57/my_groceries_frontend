1f4fa125864f59d547a5f6cf808c5159ed78c179
github username <github email address>
2020-09-07 11:29:25 -0700

    refactor render method

--- src/grocery.js
+++ src/grocery.js
@@ -11,6 +11,18 @@ class Grocery {
         Grocery.all.push(this)
     }
 
+    render() {
+        return `
+        <div data-id=${this.id}>
+        <h3>${this.market.name}<h3>
+        <p>${this.groceryItem}</p>
+        <p>${this.qty}</p>
+        <p>${this.notes}</p>
+        <button data.id=${this.id}>Edit</button>
+        </div>
+        <br><br>`;
+
+    }
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const baseUrl = "http://localhost:3000/api/v1/groceries"
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("I'm still here in index.js")
 
     getGroceries()
 
@@ -9,69 +8,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     createGroceryForm.addEventListener("submit", (e) =>
     createFormHandler(e))
-   
 })
 
-function getGroceries() {
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then(grocery => {
-        grocery.data.forEach(grocery => {
-
-            let newGrocery = new Grocery(grocery, grocery.attributes)
-        render(grocery)
+    function getGroceries() {
+        fetch(baseUrl)
+        .then(response => response.json())
+        .then(grocery => {
+            grocery.data.forEach(grocery => {
+                let newGrocery = new Grocery(grocery, grocery.attributes)
+                document.querySelector("#grocery-container").innerHTML += newGrocery.render()
+            })
         })
-    })
-}
-
-function render(grocery) {
-    const groceryMarkup = `
-        <div data-id=${grocery.id}>
-        <h3>${grocery.attributes.market.name}<h3>
-        <p>${grocery.attributes.groceryItem}</p>
-        <p>${grocery.attributes.qty}</p>
-        <p>${grocery.attributes.notes}</p>
-        <button data.id=${grocery.id}>Edit</button>
-        </div>
-        <br><br>`;
-
-    document.querySelector('#grocery-container').innerHTML += groceryMarkup
-}
+    }
 
 // function grabs all the values for the inputs on the form
-function createFormHandler(e) {
-    e.preventDefault()
-    const marketId = parseInt(document.querySelector('#markets').value)
-    const groceryItemInput = document.querySelector('#input-grocery-item').value
-    const qtyInput = document.querySelector('#input-qty').value
-    const notesInput = document.querySelector('#input-notes').value
-    postFetch(marketId, groceryItemInput, qtyInput, notesInput)
-}
+    function createFormHandler(e) {
+        e.preventDefault()
+        const marketId = parseInt(document.querySelector('#markets').value)
+        const groceryItemInput = document.querySelector('#input-grocery-item').value
+        const qtyInput = document.querySelector('#input-qty').value
+        const notesInput = document.querySelector('#input-notes').value
+        postFetch(marketId, groceryItemInput, qtyInput, notesInput)
+    }
 
-function postFetch(market_id, groceryItem, qty, notes) {
+    function postFetch(market_id, groceryItem, qty, notes) {
 
-    const bodyData = {market_id, groceryItem, qty, notes}
-    
-    fetch(baseUrl, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(bodyData)
-    })
-    .then(response => response.json())
-    .then(grocery => { 
-        console.log(grocery)
-        const groceryData = grocery.data
+        const bodyData = {market_id, groceryItem, qty, notes}
         
-        render(groceryData)
-        // const groceryMarkUp = `
-        // <div data-id=${grocery.id}>
-        //     <h3>${groceryData.market.name}</h3>
-        //     <p>${grocery.groceryItem}</p>
-        //     <p>${grocery.qty}</p>
-        //     <p>${grocery.notes}</p>
-        //     <button data-id=${groceryData.id}>Edit</button>
-        // </div>
-        // `;
-        // document.querySelector('#grocery-container').innerHTML +=groceryMarkUp
-    })
-}
+        fetch(baseUrl, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(bodyData)
+        })
+        .then(response => response.json())
+        .then(grocery => { 
+            console.log(grocery)
+            const groceryData = grocery.data
+            
+            let newGrocery = new Grocery(groceryData, groceryData.attributes)
+            document.querySelector("#grocery-container").innerHTML += newGrocery.render()
+        })
+    }
