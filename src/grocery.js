b8bd43b8a9de918fe69dcdd99ea82046cdf0e499
Lorena C <lorena@eunknown.org>
2021-04-23 16:34:02 -0700

    add css

--- src/grocery.js
+++ src/grocery.js
@@ -1,62 +1,72 @@
-console.log("Now I'm in grocery.js")
+console.log("Now I'm in grocery.js");
 
 class Grocery {
+  constructor(grocery, groceryAttributes) {
+    this.id = grocery.id;
+    this.groceryItem = groceryAttributes.groceryItem;
+    this.qty = groceryAttributes.qty;
+    this.notes = groceryAttributes.notes;
+    this.market = groceryAttributes.market;
+    Grocery.all.push(this);
+  }
 
-    constructor(grocery, groceryAttributes) {
-        this.id = grocery.id
-        this.groceryItem = groceryAttributes.groceryItem
-        this.qty = groceryAttributes.qty
-        this.notes = groceryAttributes.notes
-        this.market = groceryAttributes.market
-        Grocery.all.push(this)
-    }
+  //How does name render id .name is not part of the class
+  // render() {
+  //     return `
+  //     <div data-id=${this.id}>
+  //     <h3>${this.market.name}<h3>
+  //     <p>${this.groceryItem}</p>
+  //     <p>${this.qty}</p>
+  //     <p>${this.notes}</p>
+  //     <button id="delete-btn" data-id=${this.id}>delete</button>
+  //     <form data-id=${this.id}>
+  //         <span id="this-one">Importance: </span>
+  //         <input type="button" id='number-${this.id}' value="0" />
+  //     </form>
+  //     </div>
+  //     <br><br>`;
+  // }
 
-    //How does name render id .name is not part of the class
-    // render() {
-    //     return `
-    //     <div data-id=${this.id}>
-    //     <h3>${this.market.name}<h3>
-    //     <p>${this.groceryItem}</p>
-    //     <p>${this.qty}</p>
-    //     <p>${this.notes}</p>
-    //     <button id="delete-btn" data-id=${this.id}>delete</button>
-    //     <form data-id=${this.id}>
-    //         <span id="this-one">Importance: </span>
-    //         <input type="button" id='number-${this.id}' value="0" />
-    //     </form>
-    //     </div>
-    //     <br><br>`;
-    // }
+  render() {
+    const element = document.createElement('div');
+    element.setAttribute('data-id', this.id);
+    element.innerHTML = `
 
-render() {
-	const element = document.createElement('div');
-	element.setAttribute('data-id', this.id);
-	element.innerHTML = (`
-			<h3>${this.market.name}</h3>
-		    <p>${this.groceryItem}</p>
-		    <p>${this.qty}</p>
-		    <p>${this.notes}</p>
-		    <button id="delete-btn" data-id=${this.id}>delete</button>
+		    
 		    <form data-id=${this.id}>
 		        <span id="this-one">Importance: </span>
 		        <input type="button" id="number-${this.id}" value="0" />
 		    </form>
+
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">${this.market.name}</h5>
+    <p class="card-text">${this.groceryItem}</p>
+    <p class="card-text">${this.qty}</p>
+    <p class="card-text">${this.notes}</p>
+
+    <button type="button" id="delete-btn" class="btn btn-danger btn-sm" data-id=${this.id}>Delete</button>
+   
+  </div>
 		
-    `);
+    `;
     return element;
-}
-    //static method finds object by ID
-    static findById(id) {
-        return this.all.find(grocery => grocery.id == id)
-    }
+  }
+  //static method finds object by ID
+  static findById(id) {
+    return this.all.find((grocery) => grocery.id == id);
+  }
 
-    //static method to sort grocery object 
-    static sortGroceries(groceries) {
-        const sortedItems = groceries.sort((first, second) => first.attributes.groceryItem.toUpperCase() > second.attributes.groceryItem.toUpperCase() ? 1 : - 1
-        )
-        return sortedItems
-    }
-
+  //static method to sort grocery object
+  static sortGroceries(groceries) {
+    const sortedItems = groceries.sort((first, second) =>
+      first.attributes.groceryItem.toUpperCase() >
+      second.attributes.groceryItem.toUpperCase()
+        ? 1
+        : -1
+    );
+    return sortedItems;
+  }
 }
 
 Grocery.all = [];
--- src/index.js
+++ src/index.js
@@ -1,70 +1,71 @@
-const baseUrl = "http://localhost:3000/api/v1/groceries"
+const baseUrl = 'http://localhost:3000/api/v1/groceries';
 
 document.addEventListener('DOMContentLoaded', () => {
-    getGroceries()
-    // addNum()
+  getGroceries();
+  // addNum()
 
-    const createGroceryForm = document.querySelector("#create-grocery-form")
-    const groceryContainer = document.querySelector("#grocery-container")
-    createGroceryForm.addEventListener("submit", (e) => createFormHandler(e))
-    const sortBtn = document.getElementById('sort-button')
-    sortBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        sortGroceries()
-    })
+  const createGroceryForm = document.querySelector('#create-grocery-form');
+  const groceryContainer = document.querySelector('#grocery-container');
+  createGroceryForm.addEventListener('submit', (e) => createFormHandler(e));
+  const sortBtn = document.getElementById('sort-button');
+  sortBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    sortGroceries();
+  });
 
-    // const addOne = document.querySelector('#number')
-    // addOne.addEventListener('click', (e) => {
-    //     e.preventDefault()
-    //     addNum()
-    // });
+  // const addOne = document.querySelector('#number')
+  // addOne.addEventListener('click', (e) => {
+  //     e.preventDefault()
+  //     addNum()
+  // });
 
-    groceryContainer.addEventListener("click", (e) => {
-        const groceryId = e.target.dataset.id
-        if (e.target.id == 'delete-btn') {
-            deleteGrocery(groceryId)
-        }
-    })
-})
+  groceryContainer.addEventListener('click', (e) => {
+    const groceryId = e.target.dataset.id;
+    if (e.target.id == 'delete-btn') {
+      deleteGrocery(groceryId);
+    }
+  });
+});
 //function clears out the input field and then renders grocery objects
 function getGroceries() {
-    //debugger
-    fetch(baseUrl)
-        .then(response => response.json())
-        .then(grocery => {
-            //clears out the div 
-            document.querySelector("#grocery-container").innerHTML = ""
-            //grocery.data.forEach iterates through the array 
-            grocery.data.forEach(grocery => {
-                let newGrocery = new Grocery(grocery, grocery.attributes)
-                //The data is rendered into the div element
-                // document.querySelector("#grocery-container").innerHTML += newGrocery.render()
-                document.querySelector('#grocery-container').appendChild(newGrocery.render())
-                const addOne = document.querySelector(`#number-${newGrocery.id}`)
-                // console.log(addOne)
-                addOne.addEventListener('click', (e) => {
-                    e.preventDefault()
-                    addNum(newGrocery.id)
-                });
-            });
-        })
+  //debugger
+  fetch(baseUrl)
+    .then((response) => response.json())
+    .then((grocery) => {
+      //clears out the div
+      document.querySelector('#grocery-container').innerHTML = '';
+      //grocery.data.forEach iterates through the array
+      grocery.data.forEach((grocery) => {
+        let newGrocery = new Grocery(grocery, grocery.attributes);
+        //The data is rendered into the div element
+        // document.querySelector("#grocery-container").innerHTML += newGrocery.render()
+        document
+          .querySelector('#grocery-container')
+          .appendChild(newGrocery.render());
+        const addOne = document.querySelector(`#number-${newGrocery.id}`);
+        // console.log(addOne)
+        addOne.addEventListener('click', (e) => {
+          e.preventDefault();
+          addNum(newGrocery.id);
+        });
+      });
+    });
 }
 // function grabs all the values for the inputs on the form
 function createFormHandler(e) {
-    // console.log(new FormData(e.target))
-    e.preventDefault()
-    const marketId = parseInt(document.querySelector('#markets').value)
-    const groceryItemInput = document.querySelector('#input-grocery-item').value
-    const qtyInput = document.querySelector('#input-qty').value
-    const notesInput = document.querySelector('#input-notes').value
-    postFetch(marketId, groceryItemInput, qtyInput, notesInput)
-    clearValues()
+  // console.log(new FormData(e.target))
+  e.preventDefault();
+  const marketId = parseInt(document.querySelector('#markets').value);
+  const groceryItemInput = document.querySelector('#input-grocery-item').value;
+  const qtyInput = document.querySelector('#input-qty').value;
+  const notesInput = document.querySelector('#input-notes').value;
+  postFetch(marketId, groceryItemInput, qtyInput, notesInput);
+  clearValues();
 }
-
 
 // function createFormHandler(e) {
 //     e.preventDefault()
-   
+
 //     let groceryData = Array.from(document.querySelectorAll('#create-grocery-form input')).reduce((acc, input) => ({...acc, [input.id]: input.value}),{});
 //     postFetch(groceryData)
 //     clearValues()
@@ -72,60 +73,66 @@ function createFormHandler(e) {
 
 //function clears the values once object is submitted
 function clearValues() {
-    const marketId = document.querySelector('#markets').value = "";
-    const groceryItemInput = document.querySelector('#input-grocery-item').value = "";
-    const qtyInput = document.querySelector('#input-qty').value = "";
-    const notesInput = document.querySelector('#input-notes').value = "";
+  const marketId = (document.querySelector('#markets').value = '');
+  const groceryItemInput = (document.querySelector(
+    '#input-grocery-item'
+  ).value = '');
+  const qtyInput = (document.querySelector('#input-qty').value = '');
+  const notesInput = (document.querySelector('#input-notes').value = '');
 }
 //function post grocery object created and renders object
 function postFetch(market_id, groceryItem, qty, notes) {
-    const bodyData = { market_id, groceryItem, qty, notes }
-    fetch(baseUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(bodyData)
-    })
-        .then(response => response.json())
-        .then(grocery => {
-            console.log(grocery)
-            const groceryData = grocery.data
-            let newGrocery = new Grocery(groceryData, groceryData.attributes)
-            document.querySelector("#grocery-container").innerHTML += newGrocery.render()
-        })
+  const bodyData = { market_id, groceryItem, qty, notes };
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(bodyData),
+  })
+    .then((response) => response.json())
+    .then((grocery) => {
+      console.log(grocery);
+      const groceryData = grocery.data;
+      let newGrocery = new Grocery(groceryData, groceryData.attributes);
+      document.querySelector(
+        '#grocery-container'
+      ).innerHTML += newGrocery.render();
+    });
 }
 //function iterates through object ID and grocery attributes and renders
 function sortGroceries() {
-    document.querySelector('#grocery-container').innerHTML = ''
-    fetch(baseUrl)
-        .then(response => response.json())
-        .then(groceries => {
-            const grocerylist = Grocery.sortGroceries(groceries.data)
-            groceries.data.forEach(grocery => {
-                let findGrocery = Grocery.findById(grocery.id)
-                let newItem = new Grocery(grocery, grocery.attributes)
-                document.querySelector('#grocery-container').innerHTML += newItem.render()
-            })
-        })
+  document.querySelector('#grocery-container').innerHTML = '';
+  fetch(baseUrl)
+    .then((response) => response.json())
+    .then((groceries) => {
+      const grocerylist = Grocery.sortGroceries(groceries.data);
+      groceries.data.forEach((grocery) => {
+        let findGrocery = Grocery.findById(grocery.id);
+        let newItem = new Grocery(grocery, grocery.attributes);
+        document.querySelector(
+          '#grocery-container'
+        ).innerHTML += newItem.render();
+      });
+    });
 }
-//function deletes grocery object 
+//function deletes grocery object
 function deleteGrocery(id) {
-    fetch(`http://localhost:3000/api/v1/groceries/${id}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-    })
-        .then(response => response.json())
-        .then(grocery => {
-            // console.log(grocery)
-            Grocery.all = []
-            getGroceries()
-        })
+  fetch(`http://localhost:3000/api/v1/groceries/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .then((grocery) => {
+      // console.log(grocery)
+      Grocery.all = [];
+      getGroceries();
+    });
 }
 
 function addNum(id) {
-    let value = parseInt(document.querySelector(`#number-${id}`).value, 2);
-    console.log(value)
-    value = isNaN(value) ? 0 : value;
-    console.log(value)
-    value++;
-    document.querySelector(`#number-${id}`).value = value;
+  let value = parseInt(document.querySelector(`#number-${id}`).value, 2);
+  console.log(value);
+  value = isNaN(value) ? 0 : value;
+  console.log(value);
+  value++;
+  document.querySelector(`#number-${id}`).value = value;
 }
