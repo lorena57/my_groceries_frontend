4e838515caf4367a10a4039a5169a0d6e77b5dd6
Lorena C <lorena@eunknown.org>
2021-04-23 20:25:40 -0700

    trying to fix bug

--- src/grocery.js
+++ src/grocery.js
@@ -10,44 +10,19 @@ class Grocery {
     Grocery.all.push(this);
   }
 
-  //How does name render id .name is not part of the class
-  // render() {
-  //     return `
-  //     <div data-id=${this.id}>
-  //     <h3>${this.market.name}<h3>
-  //     <p>${this.groceryItem}</p>
-  //     <p>${this.qty}</p>
-  //     <p>${this.notes}</p>
-  //     <button id="delete-btn" data-id=${this.id}>delete</button>
-  //     <form data-id=${this.id}>
-  //         <span id="this-one">Importance: </span>
-  //         <input type="button" id='number-${this.id}' value="0" />
-  //     </form>
-  //     </div>
-  //     <br><br>`;
-  // }
-
   render() {
     const element = document.createElement('div');
     element.setAttribute('data-id', this.id);
     element.innerHTML = `
-
-		    
+			<h3>${this.market.name}</h3>
+		    <p>${this.groceryItem}</p>
+		    <p>${this.qty}</p>
+		    <p>${this.notes}</p>
+		    <button id="delete-btn" data-id=${this.id}>delete</button>
 		    <form data-id=${this.id}>
 		        <span id="this-one">Importance: </span>
 		        <input type="button" id="number-${this.id}" value="0" />
 		    </form>
-
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-    <h5 class="card-title">${this.market.name}</h5>
-    <p class="card-text">${this.groceryItem}</p>
-    <p class="card-text">${this.qty}</p>
-    <p class="card-text">${this.notes}</p>
-
-    <button type="button" id="delete-btn" class="btn btn-danger btn-sm" data-id=${this.id}>Delete</button>
-   
-  </div>
 		
     `;
     return element;
