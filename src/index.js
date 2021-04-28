85b7e0000e4b2c00f310584a48383410664f523b
Lorena C <lorena@eunknown.org>
2021-04-28 09:51:44 -0700

    change baseurl

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000/api/v1/groceries';
+const baseUrl = 'https://groceriesbackendjs.herokuapp.com/api/v1/groceries';
 
 document.addEventListener('DOMContentLoaded', () => {
   getGroceries();
